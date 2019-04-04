9552157b843aad76555866373849755eb8f29239
igorSobayev <isobayev@gmail.com>
2019-04-04 18:20:20 +0200

    Empiezo a modificar la web para dejarla como de verdad me gustaria, empiezo modificando el header cambiando la disposición del menú y añadiendo algunos estilos animados al título del header, cambio de logo y tengo pensado crear una animación del logo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { BuscarPorCriteriosComponent } from './componentes/buscar-por-criterios/buscar-por-criterios.component';
 import { JuegosComponent } from './componentes/juegos/juegos.component';
 import { IndiceComponent } from './componentes/indice/indice.component';
+import { AdministracionComponent } from './componentes/administracion/administracion.component';
 
 const routes: Routes = [
   { path: '', component: IndiceComponent },
   { path: 'buscarCriterio', component: BuscarPorCriteriosComponent },
   { path: 'juegos', component: JuegosComponent},
-  { path: 'indice', component: IndiceComponent }
+  { path: 'indice', component: IndiceComponent },
+  { path: 'administracion', component: AdministracionComponent }
   
 ];
 
